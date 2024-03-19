8e4b500dee0e49f21cfc35e5bf1dcc20406465b8
agungwahyuprayogo <101789879+agungwahyuprayogo@users.noreply.github.com>
2024-03-19 09:12:07 +0700

    2 menggunakan while.py

--- 28 -- latihan perulangan/2 menggunakan while.py
+++ 28 -- latihan perulangan/2 menggunakan while.py
@@ -1,11 +1,11 @@
 sisi = int(input("Masukan panjang sisi : ")) # masukin dulu angka paling gedenya berape
 
 count = 1 # kudu buat variable count baru lagi, kalo ga dibuat nanti cuman muncul 1 ketika di run
-while True:
+while True: # kenapa while true? biar jalan terus bre, kalo ga dibikin true dan ga ada kondisinya nanti ga jalan. sengaja dibikin while true biar jalan terus
     print(" *"*count)
     count += 1
 
     if count > sisi: # kenapa make > ? karena kalo make == nanti kurang satu bintang
-        break
+        break # nah kalo count udah sebanyak sisi, maka akan berhenti
 
 print("awal program while".center(20,"-"))
