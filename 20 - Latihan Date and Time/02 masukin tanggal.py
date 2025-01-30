eb822c258b5658e328fc77840ef8345c40554cbe
AgungWP <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-01-30 13:40:28 +0700

    02 masukin tanggal.py

--- 20 - Latihan Date and Time/02 masukin tanggal.py
+++ 20 - Latihan Date and Time/02 masukin tanggal.py
@@ -16,12 +16,23 @@ bakal error kalo masukin 05
 """
 
 intah = int(input("Masukan tahun : "))
+# input tahun
 inbul = int(input("Masukan bulan : "))
+# input bulan
 intangg = int(input("Masukan tanggal : "))
+# input tanggal
 
 tanggal = dt.date(intah,inbul,intangg)
 print(f"tanggal yang anda masukan adalah \n'{tanggal:%c}' atau normalnya {tanggal:%x}, jatuh pada hari {tanggal:%A}, {tanggal:%d} {tanggal:%B} tahun {tanggal:%Y}")
 
 """
 https://www.w3schools.com/python/python_datetime.asp
+
+'{tanggal:%c}' menampilkan tanggal dan waktu dalam format lengkap.
+'{tanggal:%x}' menampilkan tanggal dalam format standar lokal.
+'{tanggal:%A}' menampilkan nama hari dalam minggu.
+'{tanggal:%d}' menampilkan hari dalam bulan (dua digit).
+'{tanggal:%B}' menampilkan nama bulan penuh.
+'{tanggal:%Y}' menampilkan tahun dengan empat digit.
+
 """
