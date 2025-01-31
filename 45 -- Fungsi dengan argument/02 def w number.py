f7bbe2518b153b697efc6ac3aef2379088c74c9c
AgungWP <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-01-31 10:59:49 +0700

    02 def w number.py

--- 45 -- Fungsi dengan argument/02 def w number.py
+++ 45 -- Fungsi dengan argument/02 def w number.py
@@ -8,3 +8,13 @@ penjumlahan(20,30)
 30 masuk ke angka2
 nah dua angka tadi ditambah di dalam fungsi penjumlahan
 """
+
+print("pengurangan".center(25,"-"))
+
+iak1 = int(input("masukan angka pertama : "))
+iak2 = int(input("masukan angka kedua "))
+
+def pengurangan(iak1, iak2):
+    print(f"hasil dari {iak1} - {iak2} = {iak1-iak2}")
+
+pengurangan(iak1, iak2)
